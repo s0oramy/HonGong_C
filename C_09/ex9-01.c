a1c8bdd9a76df06fdb06848363ae676da18226fd
s0oramy <soo031100@naver.com>
2022-01-26 16:56:21 +0900

    주소 연산자: &

---
+++ C_09/ex9-01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    double b;
+    char c;
+
+    printf("int형 주소: %u\n", &a);
+    printf("double형 주소: %u\n", &b);
+    printf("char형 주소: %u\n", &c);
+
+    return 0;
+}
