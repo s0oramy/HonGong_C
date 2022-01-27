ccbe22ae5df23281f37d47bf334c6f6ac4435092
s0oramy <soo031100@naver.com>
2022-01-27 15:39:05 +0900

    포인터 없이 두 변수의 값 변환(error)

---
+++ C_09/ex9-08.c
@@ -0,0 +1,24 @@
+//포인터 없이 두 변수의 값을 바꾸는 함수는 불가능할까? -> NO
+
+#include <stdio.h>
+
+void swap(void);
+
+int main(void)
+{
+    int a = 10, b = 20;
+
+    swap();
+    printf("a:%d, b:%d\n", a, b);
+
+    return 0;
+}
+
+void swap(void)
+{
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+}
