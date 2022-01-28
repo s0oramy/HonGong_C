c77f998dc3e1a4464ab61a01937773669c809519
s0oramy <soo031100@naver.com>
2022-01-28 13:04:17 +0900

    포인터를 통한 배열의 값 출력

--- C_10/ex10-03.c
+++ C_10/ex10-03.c
@@ -10,7 +10,7 @@ int main(void)
     for (i = 0; i < 3; i++)
     {
 	printf("%d  ", *pa); //pa가 가리키는 배열 요소 출력
-	pa++;               //다음 배열 요소를 가리키도록 pa 값 증가
+	pa++;               //다음 배열 요소를 가리키도록 pa 값 증가, *(pa++)
     }
 
     return 0;
