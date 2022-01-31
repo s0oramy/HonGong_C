cc92b2f562555f3e967d25b0d223184c8cbe2dd1
s0oramy <soo031100@naver.com>
2022-01-31 12:48:12 +0900

    입력 버퍼 지우기

---
+++ C_11/ex11-07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num, grade;
+
+    printf("학번 입력: ");
+    scanf("%d", &num);
+    getchar();            //버퍼에 남아있는 개행 문자(\n) 제거
+    printf("학점 입력: ");
+    grade = getchar();
+    printf("학번: %d, 학점: %c", num, grade);
+
+    return 0;
+}
