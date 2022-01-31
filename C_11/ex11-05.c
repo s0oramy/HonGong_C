7e9c1a71acfb0846f5f8b1205260a6a25468262c
s0oramy <soo031100@naver.com>
2022-01-31 12:24:41 +0900

    scanf 함수 반환값 활용

--- C_11/ex11-05.c
+++ C_11/ex11-05.c
@@ -7,9 +7,9 @@ int main(void)
 
     while(1)
     {
-	res = scanf("%c", &ch);
-	if (res == -1) break;
-	printf("%d ", ch);
+	res = scanf("%c", &ch); 
+	if (res == -1) break;  //반환값이 -1이면 반복 종료
+	printf("%d ", ch);     //입력된 문자의 아스키코드 값 출력
     }
 
     return 0;
