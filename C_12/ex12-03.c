4a23a1e75920f6fbed508165c0812e16e3d53af1
s0oramy <soo031100@naver.com>
2022-02-07 17:30:46 +0900

    gets 함수를 사용한 문자열

---
+++ C_12/ex12-03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char str[80];
+
+    printf("insert: ");
+    scanf("%s", str);
+    printf("first word: %s\n", str);
+    scanf("%s", str);
+    printf("버퍼에 남아 있는 second word: %s\n", str);
+
+    return 0;
+}
