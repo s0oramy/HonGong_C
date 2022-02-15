dc61ff18b86e61f8772e822c51d56039bbe55f0e
s0oramy <soo031100@naver.com>
2022-02-15 11:44:55 +0900

    확인문제_p120

---
+++ C_prac/4-07.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double kg, cm, bmi;
+    int res;
+
+    printf("몸무게(kg)와 키(cm)입력: ");
+    scanf("%lf %lf", &kg, &cm);
+    bmi = kg / (cm * cm);
+
+    res = (20.0 <= bmi) && (bmi < 25.0);
+    printf("%s\n", (res == 1) ? "표준입니다":"체중관리가 필요");
+
+    return 0;
+}
