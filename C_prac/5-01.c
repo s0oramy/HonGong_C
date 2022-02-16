27a2bbff9c12518dd7618c1ef6f4b31f03b8e79d
s0oramy <soo031100@naver.com>
2022-02-16 11:02:55 +0900

    확인문제_p135

---
+++ C_prac/5-01.c
@@ -0,0 +1,27 @@
+/* 01 */
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+
+    if(a>0)
+    {
+	a = -a;
+    }
+
+/* 02 */
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+
+    if((a % 2) == 0)
+    {
+	a = 2;
+    }
+    else
+    {
+	a = 1;
+    }
