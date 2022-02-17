60d9c7ca31866603a13ba40f66664d8f1c517fb8
s0oramy <soo031100@naver.com>
2022-02-17 16:06:32 +0900

    확인문제_p165

---
+++ C_prac/6-01.c
@@ -0,0 +1,25 @@
+/*01*/
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+
+    do
+    {
+	scanf("%d", &a);
+    }while(a < 0);
+}
+
+/*02*/
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    for(i = 0;i < 5;i++)
+    {
+	printf("Be Happy\n");
+    }
+}
