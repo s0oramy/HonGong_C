ddf5b5ce75c4ec607d9e0175014eff8f53f551fa
s0oramy <soo031100@naver.com>
2022-02-21 16:45:52 +0900

    확인문제_p191

---
+++ C_prac/7-02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+double centi_to_meter(int cm);
+
+int main(void){
+    double res;
+
+    res = centi_to_meter(187);
+    printf("%.2lfm\n", res);
+
+    return 0;
+}
+
+double centi_to_meter(int cm){
+    double m;
+    m = cm / 100.0;
+    return m;
+}
