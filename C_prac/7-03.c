8d5976eb69448fd23611aaf5bc5de07f05b8789a
s0oramy <soo031100@naver.com>
2022-02-22 18:26:26 +0900

    확인무제_p205

---
+++ C_prac/7-03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sum(int a);
+
+int main(void){
+    sum(10);
+    sum(100);
+    return 0;
+}
+
+int sum(int a){
+    int i, result = 0;
+    for(i=1; i<= a; i++){
+	result += i;
+    } 
+printf("1부터 %d까지의 합은 %d입니다.\n", a, result);
+}
